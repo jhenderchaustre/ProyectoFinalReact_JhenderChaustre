f7dc6283ddd48213df0cc3b69ee268ba87833da7
jhenderchaustre <jhenderchaustrear@gmail.com>
2023-12-17 21:07:34 -0300

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from "./components/ItemListContainer";
  const App = () => {
